3daf0590b9d46eec695cf62c01955c616e2ec467
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-07-06 09:54:57 +0100

    modifier format document web Storm

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,102 +6,85 @@ const nodemailer = require("nodemailer");
 const maxAge = 2 * 60 * 60; // 2 heures
 
 const createToken = (id) => {
-  return jwt.sign({ id }, "net attijari secret", {
-    expiresIn: maxAge,
-  });
+    return jwt.sign({id}, "net attijari secret", {
+        expiresIn: maxAge,
+    });
 };
 
 module.exports.signup_post = async (req, res) => {
-  const { filename } = req.file;
-  const { email, password, username } = req.body;
-  try {
-    const user = await userModel.create({
-      username,
-      password,
-      email,
-      image_user: filename,
-    });
-    // Envoi de l'e-mail à l'adresse e-mail de l'utilisateur
-    sendWelcomeEmail(email, username);
-    const token = createToken(user._id);
-    res.cookie("jwt_token", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    const {filename} = req.file;
+    const {email, password, username} = req.body;
+    try {
+        const user = await userModel.create({
+            username, password, email, image_user: filename,
+        });
+        // Envoi de l'e-mail à l'adresse e-mail de l'utilisateur
+        sendWelcomeEmail(email, username);
+        const token = createToken(user._id);
+        res.cookie("jwt_token", token, {httpOnly: true, maxAge: maxAge * 1000});
+        res.status(201).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 };
 
 module.exports.activation = async (req, res) => {
-  try {
-    const email = req.query.email;
-    const checkIfUserExists = await userModel.findOne({ email });
+    try {
+        const email = req.query.email;
+        const checkIfUserExists = await userModel.findOne({email});
 
-    if (!checkIfUserExists) {
-      throw new Error("User not found!");
+        if (!checkIfUserExists) {
+            throw new Error("User not found!");
+        }
+        if (checkIfUserExists.enabled) {
+            return res.redirect("http://localhost:3000/login-page/?message=Utilisateur_est_deja_active");
+        }
+
+        const currentDate = new Date();
+        const updatedUser = await userModel.findByIdAndUpdate(checkIfUserExists._id, {
+                $set: {
+                    enabled: true, updated_at: currentDate,
+                },
+            }, {new: true} // Set the { new: true } option to return the updated user
+        );
+
+        return res.redirect("http://localhost:3000/login-page/?message=Utilisateur_activee_Avec_succes");
+    } catch (error) {
+        return res.status(500).json({message: error.message});
     }
-    if (checkIfUserExists.enabled) {
-      return res.redirect("http://localhost:3000/login-page/?message=Utilisateur_est_deja_active");
-    }
-
-    const currentDate = new Date();
-    const updatedUser = await userModel.findByIdAndUpdate(
-        checkIfUserExists._id,
-        {
-          $set: {
-            enabled: true,
-            updated_at: currentDate,
-          },
-        },
-        { new: true } // Set the { new: true } option to return the updated user
-    );
-
-    return res.redirect("http://localhost:3000/login-page/?message=Utilisateur_activee_Avec_succes");
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
 };
 
 
-
-
 module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await userModel.login(email, password);
-    const token = createToken(user._id);
-    res.cookie("jwt_token", token, { httpOnly: false, maxAge: maxAge * 1000 });
-    req.session.user = user;
-    // console.log(req.session);
-    res.status(200).json({
-      message: "User successfully authenticated",
-      user: user,
-    });
-  } catch (error) {
-    res
-      .status(400)
-      .json({
-        erreur: error.message,
-      });
-  }
+    const {email, password} = req.body;
+    try {
+        const user = await userModel.login(email, password);
+        const token = createToken(user._id);
+        res.cookie("jwt_token", token, {httpOnly: false, maxAge: maxAge * 1000});
+        req.session.user = user;
+        // console.log(req.session);
+        res.status(200).json({
+            message: "User successfully authenticated", user: user,
+        });
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            erreur: error.message,
+        });
+    }
 };
 
 // Fonction pour envoyer un e-mail de bienvenue à l'utilisateur
 function sendWelcomeEmail(email, username, id) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "greencrowd2223@gmail.com",
-      pass: "zothevkvkhobyyzw",
-    },
-  });
-  const activationLink = `http://localhost:5000/auth/validation?email=${encodeURIComponent(
-    email
-  )}`;
-  const mailOptions = {
-    from: "greencrowd2223@gmail.com",
-    to: email,
-    subject: "Bienvenue sur notre site",
-    html: `
+    const transporter = nodemailer.createTransport({
+        service: "gmail", auth: {
+            user: "greencrowd2223@gmail.com", pass: "zothevkvkhobyyzw",
+        },
+    });
+    const activationLink = `http://localhost:5000/auth/validation?email=${encodeURIComponent(email)}`;
+    const mailOptions = {
+        from: "greencrowd2223@gmail.com", to: email, subject: "Bienvenue sur notre site", html: `
       <html>
         <head>
           <style>
@@ -151,276 +134,229 @@ function sendWelcomeEmail(email, username, id) {
         </body>
       </html>
     `,
-  };
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.error("Erreur lors de l'envoi de l'e-mail de bienvenue :", error);
-    } else {
-      console.log("E-mail de bienvenue envoyé avec succès !");
-    }
-  });
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.error("Erreur lors de l'envoi de l'e-mail de bienvenue :", error);
+        } else {
+            console.log("E-mail de bienvenue envoyé avec succès !");
+        }
+    });
 }
 
 module.exports.logout = (req, res) => {
-  try {
-    res.cookie("jwt_token", "", { httpOnly: false, maxAge: 1 });
-    req.session.destroy();
-    res.status(200).json({
-      message: "User successfully authenticated",
-    });
-  } catch (error) {
-    res.status(400).json({ erreur: error.message });
-  }
+    try {
+        res.cookie("jwt_token", "", {httpOnly: false, maxAge: 1});
+        req.session.destroy();
+        res.status(200).json({
+            message: "User successfully authenticated",
+        });
+    } catch (error) {
+        res.status(400).json({erreur: error.message});
+    }
 };
 
 module.exports.getUsers = async (req, res, next) => {
-  try {
-    const users = await userModel.find();
-    if (!users || users.length === 0) {
-      throw new Error("users not found !");
+    try {
+        const users = await userModel.find();
+        if (!users || users.length === 0) {
+            throw new Error("users not found !");
+        }
+        res.status(200).json({users});
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    res.status(200).json({ users });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 module.exports.getUser = async (req, res, next) => {
-  try {
-    const id = req.session.user._id.toString();
-    const user = await userModel.findById(id);
-    if (!user || user.length === 0) {
-      throw new Error("users not found !");
+    try {
+        const id = req.session.user._id.toString();
+        const user = await userModel.findById(id);
+        if (!user || user.length === 0) {
+            throw new Error("users not found !");
+        }
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 module.exports.UserByID = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const user = await userModel.findById(id);
-    if (!user || user.length === 0) {
-      throw new Error("users not found !");
+    try {
+        const id = req.params.id;
+        const user = await userModel.findById(id);
+        if (!user || user.length === 0) {
+            throw new Error("users not found !");
+        }
+        res.status(200).json({user});
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 module.exports.addUser = async (req, res, next) => {
-  try {
-    const { filename } = req.file;
-    console.log("filename", req.file);
-    const {
-      username,
-      password,
-      email,
-      first_Name,
-      last_Name,
-      phoneNumber,
-      userType,
-    } = req.body;
-    console.log(req.body);
-    const user = new userModel({
-      username,
-      password,
-      email,
-      first_Name,
-      last_Name,
-      phoneNumber,
-      userType,
-      image_user: filename,
-    });
+    try {
+        const {filename} = req.file;
+        console.log("filename", req.file);
+        const {
+            username, password, email, first_Name, last_Name, phoneNumber, userType,
+        } = req.body;
+        console.log(req.body);
+        const user = new userModel({
+            username, password, email, first_Name, last_Name, phoneNumber, userType, image_user: filename,
+        });
 
-    const addeduser = await user.save();
+        const addeduser = await user.save();
 
-    res.status(200).json(addeduser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        res.status(200).json(addeduser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 };
 module.exports.updateUser = async (req, res, next) => {
-  try {
-    const { first_Name, last_Name, phoneNumber, password } = req.body;
-    console.log(req.body);
-    const id = req.user._id.toString();
-    console.log(req.user._id.toString());
-    const checkIfusertExists = await userModel.findById(id);
-    if (!checkIfusertExists) {
-      throw new Error("user not found !");
+    try {
+        const {first_Name, last_Name, phoneNumber, password} = req.body;
+        console.log(req.body);
+        const id = req.user._id.toString();
+        console.log(req.user._id.toString());
+        const checkIfusertExists = await userModel.findById(id);
+        if (!checkIfusertExists) {
+            throw new Error("user not found !");
+        }
+        const currentDate = new Date();
+        updateedUser = await userModel.findByIdAndUpdate(id, {
+            $set: {
+                password, first_Name, last_Name, phoneNumber, updated_at: currentDate,
+            },
+        }, {new: true});
+        res.status(200).json(updateedUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    const currentDate = new Date();
-    updateedUser = await userModel.findByIdAndUpdate(
-      id,
-      {
-        $set: {
-          password,
-          first_Name,
-          last_Name,
-          phoneNumber,
-          updated_at: currentDate,
-        },
-      },
-      { new: true }
-    );
-    res.status(200).json(updateedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 module.exports.deleteUser = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await userModel.findById(id);
+    try {
+        const {id} = req.params;
+        const user = await userModel.findById(id);
 
-    if (!user) {
-      return res.status(404).json({ message: "user not found!" });
+        if (!user) {
+            return res.status(404).json({message: "user not found!"});
+        }
+
+        await userModel.findByIdAndDelete(user._id);
+
+        res.status(200).json("deleted");
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-
-    await userModel.findByIdAndDelete(user._id);
-
-    res.status(200).json("deleted");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 module.exports.forgotpwd = async (req, res) => {
-  const { email } = req.body;
-  const URL = "http://localhost:3000/resetpwd";
+    const {email} = req.body;
+    const URL = "http://localhost:3000/resetpwd";
 
-  try {
-    res.status(200).json({ message: "Welcome" });
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: email,
-      from: "greencrowdproject@gmail.com",
-      subject: "Welcome to Green Crowd Project",
-      html: `
+    try {
+        res.status(200).json({message: "Welcome"});
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: email, from: "greencrowdproject@gmail.com", subject: "Welcome to Green Crowd Project", html: `
 				<h2>Click the link to reset your password</h2>
 				<p>${URL}</p>
-			`,
-      //templateId: 'd-e09cf57a0a0e45e894027ffd5b6caebb',
-    };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+			`, //templateId: 'd-e09cf57a0a0e45e894027ffd5b6caebb',
+        };
+        sgMail
+        .send(msg)
+        .then(() => {
+            console.log("Email sent");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 };
 
 module.exports.upgrade = async (req, res) => {
-  try {
-    console.log("test", req.bod);
+    try {
+        console.log("test", req.bod);
 
-    const { id } = req.body; // Récupération de l'ID depuis le corps de la requête
-    const checkIfUserExists = await userModel.findById(id);
+        const {id} = req.body; // Récupération de l'ID depuis le corps de la requête
+        const checkIfUserExists = await userModel.findById(id);
 
-    if (!checkIfUserExists) {
-      throw new Error("User not found!");
+        if (!checkIfUserExists) {
+            throw new Error("User not found!");
+        }
+
+        const currentDate = new Date();
+        const userType = "admin";
+
+        const updatedUser = await userModel.findByIdAndUpdate(id, {
+            $set: {
+                userType: userType, enabled: true, updated_at: currentDate,
+            },
+        }, {new: true});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-
-    const currentDate = new Date();
-    const userType = "admin";
-
-    const updatedUser = await userModel.findByIdAndUpdate(
-      id,
-      {
-        $set: {
-          userType: userType,
-          enabled: true,
-          updated_at: currentDate,
-        },
-      },
-      { new: true }
-    );
-
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 module.exports.downgrade = async (req, res) => {
-  try {
-    const {id} = req.body;
-    const checkIfusertExists = await userModel.findById(id);
-    if (!checkIfusertExists) {
-      throw new Error("user not found !");
+    try {
+        const {id} = req.body;
+        const checkIfusertExists = await userModel.findById(id);
+        if (!checkIfusertExists) {
+            throw new Error("user not found !");
+        }
+        const currentDate = new Date();
+        const user = "user";
+        updateedUser = await userModel.findByIdAndUpdate(id, {
+            $set: {
+                userType: user, enabled: true, updated_at: currentDate,
+            },
+        }, {new: true});
+        res.status(200).json(updateedUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    const currentDate = new Date();
-    const user = "user";
-    updateedUser = await userModel.findByIdAndUpdate(
-      id,
-      {
-        $set: {
-          userType: user,
-          enabled: true,
-          updated_at: currentDate,
-        },
-      },
-      { new: true }
-    );
-    res.status(200).json(updateedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 
-module.exports.Desactive= async (req, res) => {
-  try {
-    const {id} = req.body;
-    const checkIfusertExists = await userModel.findById(id);
-    if (!checkIfusertExists) {
-      throw new Error("user not found !");
+module.exports.Desactive = async (req, res) => {
+    try {
+        const {id} = req.body;
+        const checkIfusertExists = await userModel.findById(id);
+        if (!checkIfusertExists) {
+            throw new Error("user not found !");
+        }
+        const currentDate = new Date();
+        updateedUser = await userModel.findByIdAndUpdate(id, {
+            $set: {
+                enabled: false, updated_at: currentDate,
+            },
+        }, {new: true});
+        res.status(200).json(updateedUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    const currentDate = new Date();
-    updateedUser = await userModel.findByIdAndUpdate(
-      id,
-      {
-        $set: {
-          enabled: false,
-          updated_at: currentDate,
-        },
-      },
-      { new: true }
-    );
-    res.status(200).json(updateedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
 
 module.exports.Active = async (req, res) => {
-  try {
-    const {id} = req.body;
-    const checkIfusertExists = await userModel.findById(id);
-    if (!checkIfusertExists) {
-      throw new Error("user not found !");
+    try {
+        const {id} = req.body;
+        const checkIfusertExists = await userModel.findById(id);
+        if (!checkIfusertExists) {
+            throw new Error("user not found !");
+        }
+        const currentDate = new Date();
+        updateedUser = await userModel.findByIdAndUpdate(id, {
+            $set: {
+                enabled: true, updated_at: currentDate,
+            },
+        }, {new: true});
+        res.status(200).json(updateedUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
-    const currentDate = new Date();
-    updateedUser = await userModel.findByIdAndUpdate(
-      id,
-      {
-        $set: {
-          enabled: true,
-          updated_at: currentDate,
-        },
-      },
-      { new: true }
-    );
-    res.status(200).json(updateedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };

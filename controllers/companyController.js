db07bf2cb092edd49c447e2d7de3870f80f93444
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-07-16 00:34:03 +0100

    code complet crud company

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,8 +1,7 @@
 const CompanySchema = require('../models/CompanySchema')
-const userModel = require('../models/userSchema')
 
 // Récupérer la liste des entreprises
-module.exports.getCompanies = async (req, res) => {
+module.exports.getCompanies = async (req, res ,next) => {
   try {
     const Companys = await CompanySchema.find()
     if (!Companys || Companys.length === 0) {
@@ -15,56 +14,58 @@ module.exports.getCompanies = async (req, res) => {
 }
 
 // Créer une nouvelle entreprise
-module.exports.createCompany = async (req, res) => {
+module.exports.createCompany = async (req, res,next) => {
   const companyName = req.body.companyName
   const filePath = req.file.path
-
-  const newCompany = new Company({
-    nom: companyName,
-    fichierExcel: filePath
-  })
-
-  newCompany.save((err) => {
-    if (err) {
-      console.error(err)
-      res.status(500).send('Erreur du serveur')
-    } else {
-      res.send('Entreprise créée avec succès')
-    }
-  })
+  try {
+    const Company = await CompanySchema.create({
+      nom: companyName,
+      fichierExcel: filePath
+    })
+    res.status(201).json({ Company })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 // Mettre à jour une entreprise
-module.exports.updateCompany = async (req, res) => {
-  const companyId = req.params.id
-  const updatedCompanyName = req.body.companyName
-
-  Company.findByIdAndUpdate(companyId, { nom: updatedCompanyName }, (err, company) => {
-    if (err) {
-      console.error(err)
-      res.status(500).send('Erreur du serveur')
-    } else if (!company) {
-      res.status(404).send('Entreprise non trouvée')
-    } else {
-      res.send('Entreprise mise à jour avec succès')
+module.exports.updateCompany = async (req, res,next) => {
+  try {
+    const companyId = req.params.id
+    const companyName = req.body.companyName
+    const checkIfcompanyExists = await CompanySchema.findById(id);
+    if ( !checkIfcompanyExists) {
+      throw new Error("company not found !");
     }
-  })
-}
+    // const currentDate = new Date();
+    updateedcompany = await CompanySchema.findByIdAndUpdate(companyId, {
+      $set: {
+        companyName      },
+    }, {new: true});
+    res.status(200).json(updateedcompany);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
+
 
 // Supprimer une entreprise
-const deleteCompany = async (req, res) => {
-  const companyId = req.params.id
+module.exports.deleteCompany = async (req, res,next) => {
+    try {
+    const {id} = req.params;
+    const company = await CompanySchema.findById(id);
 
-  Company.findByIdAndDelete(companyId, (err, company) => {
-    if (err) {
-      console.error(err)
-      res.status(500).send('Erreur du serveur')
-    } else if (!company) {
-      res.status(404).send('Entreprise non trouvée')
-    } else {
-      res.send('Entreprise supprimée avec succès')
+    if ( !company) {
+      return res.status(404).json({message: "company not found!"});
     }
-  })
-}
+
+    await CompanySchema.findByIdAndDelete(company._id);
+
+    res.status(200).json("deleted");
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
+
 
 

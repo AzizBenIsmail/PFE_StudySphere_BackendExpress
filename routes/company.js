a405416d5f3b49a20f2662599d90ede617b779b6
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-07-16 00:33:40 +0100

    résoudre l erreur du routage company

--- routes/company.js
+++ routes/company.js
@@ -1,13 +1,15 @@
-var express = require("express");
-var router = express.Router();
-const uploadXcl = require("../middlewares/uploadXcl");
-const Company = require("../controllers/companyController");
-const { requireAuthUser } = require("../middlewares/authMiddleware");
+var express = require('express')
+var router = express.Router()
+const uploadXcl = require('../middlewares/uploadXcl')
+const Company = require('../controllers/companyController')
+// const { requireAuthUser } = require("../middlewares/authMiddleware");
 
-router.get('/companies',);
+router.get('/companies', Company.getCompanies)
 
-router.post('/companies', uploadXcl.single('excelFile'),);
+router.post('/companies', uploadXcl.single('excelFile'), Company.createCompany)
 
-router.put('/companies/:id',);
+router.put('/companies/:id', Company.updateCompany)
 
-router.delete('/companies/:id',);
+router.delete('/companies/:id', Company.deleteCompany)
+
+module.exports = router;

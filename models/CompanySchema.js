314f70eca1de0c291f3fbb9b9a2067afecc1f93c
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-07-17 22:37:02 +0100

    réglage du schéma de compagne

--- models/CompanySchema.js
+++ models/CompanySchema.js
@@ -1,10 +1,32 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const companySchema = new mongoose.Schema({
   nomCompagne: String,
   fichierExcel: String,
   image_Compagne: String,
+  validation: Boolean,
+  createdAt: Date,
+  updatedAt: Date,
+  contacts: [
+    {
+      nom: String,
+      email: String,
+      dateEnvoi: Date
+    }
+  ]
+})
 
+companySchema.pre('save', async function (next) {
+  try {
+    const Company = this
+    Company.createdAt = new Date()
+    Company.updatedAt = new Date()
+    Company.validation = false //false
+    next()
+  } catch (error) {
+    next(error)
+  }
 })
 
 const Company = mongoose.model('Company', companySchema)

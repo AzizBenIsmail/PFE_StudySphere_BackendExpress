8880abf89904df70d8a461f566d2148b93fb17a8
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-21 23:13:44 +0100

    ajouter préférence pour un centre et récupérer un centre

--- routes/pref.js
+++ routes/pref.js
@@ -9,6 +9,9 @@ const { centreValidation } = require("../middlewares/centreValidation");
 /* GET user by ID. */
 router.put("/addPref/:id", pref.AddPreferences);
 
+router.put("/addPrefCentre/:id", pref.AddPreferencesCentre);
+
+router.get("/GetPref/:id", pref.getPreferencesById);
 
 
 module.exports = router;

631b09d76a5e25b16bb98bc0883ba2b9707018d6
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-24 18:07:02 +0100

    ajouter route badge

---
+++ routes/badge.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+const badge = require("../controllers/badgeControllers");
+const upload = require("../middlewares/upload");
+
+// Routes pour les badges
+router.post("/",upload.single("image_user") , badge.createBadge);
+router.get("/", badge.getAllBadges);
+router.get("/:id", badge.getBadgeById);
+router.put("/:id",upload.single("image_user"), badge.updateBadge);
+router.delete("/:id", badge.deleteBadge);
+
+
+module.exports = router;

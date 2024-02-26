89d77a0df416229fa9b54079393973859e87a1e0
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-26 17:01:24 +0100

    élimination source

--- models/xpSchema.js
+++ models/xpSchema.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const xpSchema = new mongoose.Schema({
   pointsGagnes: {type: Number,required: true},
   // date: {type: Date,default: Date.now},
-  source: {type: String,enum: ['connexion quotidienne', 'achèvement d\'un cours', 'autre'], required: true},
   niveauAtteint: { type: mongoose.Schema.Types.ObjectId, ref: 'Niveau', required: true }, // Référence à l'ID du niveau atteint
   badgeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Badge' }], // Références à plusieurs badges
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to User

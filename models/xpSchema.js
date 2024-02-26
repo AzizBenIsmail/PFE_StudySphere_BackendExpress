ff691a4b69d253f68212493b350074172ec7ae10
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-02-26 16:05:09 +0100

    liste des badges

--- models/xpSchema.js
+++ models/xpSchema.js
@@ -5,9 +5,8 @@ const xpSchema = new mongoose.Schema({
   // date: {type: Date,default: Date.now},
   source: {type: String,enum: ['connexion quotidienne', 'achèvement d\'un cours', 'autre'], required: true},
   niveauAtteint: { type: mongoose.Schema.Types.ObjectId, ref: 'Niveau', required: true }, // Référence à l'ID du niveau atteint
-  badgeId: {type: mongoose.Schema.Types.ObjectId,ref: 'Badge'},
+  badgeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Badge' }], // Références à plusieurs badges
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to User
-
 });
 
 const XP = mongoose.model('XP', xpSchema);

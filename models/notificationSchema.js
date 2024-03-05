87221cc2d410f2a2fc5d0185c6dc5fbcf39765a4
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-05 20:36:45 +0100

    model notification version 1.0.0

---
+++ models/notificationSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  type: { type: String,  enum: ['message', 'activity', 'update'],  },
+  read: { type: Boolean, default: false  },
+  createdAt: { type: Date, default: Date.now },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;

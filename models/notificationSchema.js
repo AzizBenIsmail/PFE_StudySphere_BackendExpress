0488f7ce174c27282c69399a08ba293d88d77843
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-06 17:04:02 +0100

    ajouter vu

--- models/notificationSchema.js
+++ models/notificationSchema.js
@@ -4,6 +4,7 @@ const notificationSchema = new mongoose.Schema({
   content: { type: String, required: true },
   type: { type: String,  enum: ['message', 'activity', 'update'],  },
   read: { type: Boolean, default: false  },
+  vu: { type: Boolean, default: false  },
   createdAt: { type: Date, default: Date.now },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
 });

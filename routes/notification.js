5518102f90b141fe6d32f4ab842357e2eb5e2dfb
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-07 15:54:36 +0100

    modification des routes

--- routes/notification.js
+++ routes/notification.js
@@ -5,11 +5,11 @@ const { requireAuthUser } = require('../middlewares/authMiddleware')
 
 router.post("/",requireAuthUser, notification.createNotification);
 router.get("/",requireAuthUser, notification.getAllNotifications);
-router.get("/notification/:userId",requireAuthUser, notification.getUserNotifications);
+router.get("/:userId",requireAuthUser, notification.getUserNotifications);
 router.get("/:id",requireAuthUser, notification.getNotificationById);
 router.put("/:id",requireAuthUser, notification.updateNotification);
 router.delete("/:id",requireAuthUser, notification.deleteNotification);
 router.put("/:id/read", requireAuthUser, notification.markNotificationAsRead);
-router.put('/:id/markAsRead', notification.markNotificationAsRead);
+router.put('/:id/markAsRead', notification.markNotificationAsvu);
 
 module.exports = router;

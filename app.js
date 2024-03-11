43e18667756ab54f13ee7cfca2f4c8faa13fd2ff
yasminaaskri <yasminaaskri7@gmail.com>
2024-03-11 10:00:51 +0100

    second step of backend chat
    correction of middleware fils

--- app.js
+++ app.js
@@ -1,127 +1,91 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const http = require("http");
-const cors = require('cors');
-const logMiddleware = require('./middlewares/authLogMiddleware');
-const session = require('express-session');
+const cors = require("cors");
+const logMiddleware = require("./middlewares/authLogMiddleware");
+const session = require("express-session");
 const multer = require("multer");
-const { fileURLToPath } = require('url');
+const { fileURLToPath } = require("url");
 const path = require("path");
-
+const { upload } = require("./middlewares/uploadFils/uploadFileChat.js");
 
 const { app, server } = require("./socket/socket.js");
 
-
-require('./controllers/init');
+require("./controllers/init");
 
 require("dotenv").config(); //configuration dotenv
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var prefRouter = require('./routes/pref');
-var usersRouter = require('./routes/users');
-var xpRouter = require('./routes/xp');
-var badgeRouter = require('./routes/badge');
-var niveauRouter = require('./routes/niveau');
-var notificationRouter = require('./routes/notification');
-var messageRouter = require('./routes/message');
-const { connectToMongoDB } = require('./db/db');
-const { default: Message } = require('./models/messageSchema');
-
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
+var prefRouter = require("./routes/pref");
+var usersRouter = require("./routes/users");
+var xpRouter = require("./routes/xp");
+var badgeRouter = require("./routes/badge");
+var niveauRouter = require("./routes/niveau");
+var notificationRouter = require("./routes/notification");
+var messageRouter = require("./routes/message");
+const { connectToMongoDB } = require("./db/db");
+const { default: Message } = require("./models/messageSchema");
 
 //var app = express();
 
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(logMiddleware);
-app.use(session({
-  secret: 'net attijari secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false, // À définir sur true si vous utilisez HTTPS
-    maxAge: 24 * 60 * 60 * 1000, // Durée de validité du cookie de session (en millisecondes)
-  },
-}));
+app.use(
+  session({
+    secret: "net attijari secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, // À définir sur true si vous utilisez HTTPS
+      maxAge: 24 * 60 * 60 * 1000, // Durée de validité du cookie de session (en millisecondes)
+    },
+  })
+);
 app.use(express.static("public"));
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials',
-  credentials: true
-}));
-
-
-
-
-
-
-// Multer configuration for handling any type of file
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Check the file type and set the destination accordingly
-    if (file.mimetype.startsWith('image')) {
-      cb(null, 'public/images/');
-    } else if (file.mimetype.startsWith('video')) {
-      cb(null, 'public/videos/');
-    } else {
-      cb(null, 'public/files/');
-    }
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders:
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials",
+    credentials: true,
+  })
+);
 
 // Debugging: Log current directory path
 const currentDirname = path.dirname(__filename);
 console.log("Directory path:", currentDirname);
 
 // Serve static files from the 'public/videos' directory
-const publicVideosDirectoryPath = path.join(currentDirname, 'public/videos');
+const publicVideosDirectoryPath = path.join(currentDirname, "public/videos");
 app.use(express.static(publicVideosDirectoryPath));
 
 // Serve static files from the 'public/files' directory
-const publicFilesDirectoryPath = path.join(currentDirname, 'public/files');
+const publicFilesDirectoryPath = path.join(currentDirname, "public/files");
 app.use(express.static(publicFilesDirectoryPath));
 
 // Serve static files from the 'public/images' directory
-const publicImagesDirectoryPath = path.join(currentDirname, 'public/images');
+const publicImagesDirectoryPath = path.join(currentDirname, "public/images");
 app.use(express.static(publicImagesDirectoryPath));
-
-
-
-
 
 // Use the upload middleware to handle any type of file
 app.use(upload.single("file"));
 
-
-
-
-
-
-
-
-
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/user', usersRouter);
-app.use('/pref', prefRouter);
-app.use('/xp', xpRouter);
-app.use('/badge', badgeRouter);
-app.use('/niveau', niveauRouter);
-app.use('/notification', notificationRouter);
-app.use('/message', messageRouter)
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/user", usersRouter);
+app.use("/pref", prefRouter);
+app.use("/xp", xpRouter);
+app.use("/badge", badgeRouter);
+app.use("/niveau", niveauRouter);
+app.use("/notification", notificationRouter);
+app.use("/message", messageRouter);
 
 // ...
 

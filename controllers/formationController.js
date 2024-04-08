9dc68537e7bfbf25cb0505821fea9a1a419eef49
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-04-08 19:21:29 +0100

    affecter une formation a user

--- controllers/formationController.js
+++ controllers/formationController.js
@@ -1,17 +1,42 @@
 const Formation = require('../models/formationSchema');
+const User = require('../models/userSchema'); // Importer le modèle d'utilisateur
 
-// Controller pour créer une nouvelle formation
 exports.createFormation = async (req, res) => {
   try {
     const { filename } = req.file; // Récupérer le nom du fichier de l'image
     const formationData = { ...req.body, image_Formation: filename }; // Ajouter le nom du fichier au corps de la requête
+
+    // Récupérer les ID du formateur et du centre à partir du corps de la requête
+    const { formateur, centre } = req.body;
+
+    // Rechercher le formateur et le centre dans la base de données
+    const Userformateur = await User.findById(formateur);
+    const Usercentre = await User.findById(centre);
+
+    // Vérifier si le formateur et le centre existent
+    if (!Userformateur || !Usercentre) {
+      return res.status(404).json({ success: false, error: 'Formateur or centre not found' });
+    }
+
+    // Associer le formateur et le centre à la formation
+    formationData.formateur = Userformateur._id;
+    formationData.centre = Usercentre._id;
+
+    // Créer la formation avec les données fournies
     const formation = await Formation.create(formationData);
+
+    // Ajouter la formation créée aux formateurs et centres respectifs
+    Userformateur.Formations.push(formation._id);
+    Usercentre.Formations.push(formation._id);
+    await Userformateur.save();
+    await Usercentre.save();
+
     res.status(201).json({ formation });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
 };
-// Controller pour récupérer toutes les formations
+
 exports.getFormations = async (req, res) => {
   try {
     const formations = await Formation.find().populate("centre").populate("formateur");

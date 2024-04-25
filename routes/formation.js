24f66ac1519fd9942e87414c336a842bb5e7e6d9
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-04-25 16:20:59 +0100

    protection du route formation

--- routes/formation.js
+++ routes/formation.js
@@ -10,7 +10,7 @@ router.get('/', formationController.getFormations); // Récupérer toutes les fo
 router.get('/FormationByCentre',requireAuthUser , formationController.getFormationsByCentre); // Récupérer toutes les formations
 router.get('/FormationByIdCentre/:id',requireAuthUser , formationController.getFormationsById); // Récupérer toutes les formations
 router.get('/FormationByIdFormateur/:id',requireAuthUser , formationController.getFormationByIdFormateur); // Récupérer toutes les formations
-router.get('/:id', formationController.getFormationById); // Récupérer une formation par son ID
+router.get('/:id',requireAuthUser, formationController.getFormationById); // Récupérer une formation par son ID
 router.put('/:id', requireAuthUser ,upload.single("image_Formation"),formationController.updateFormation); // Mettre à jour une formation
 router.delete('/:id', requireAuthUser ,formationController.deleteFormation); // Supprimer une formation
 

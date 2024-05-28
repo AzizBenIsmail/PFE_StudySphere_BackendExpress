3adf170c10b6b54ba788d4b5a6b85ac57f5f4b76
yasminaaskri <yasminaaskri7@gmail.com>
2024-05-28 11:53:18 +0100

    ajout for sidebar

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -779,3 +779,16 @@ module.exports.desinfecterFormateur = async (req, res) => {
     res.status(500).json({ message: "Erreur serveur lors du retrait de l'enseignant du staff enseignant" });
   }
 }
+
+module.exports.getUsersForSidebar = async (req, res) => {
+	try {
+		const loggedInUserId = req.session.user._id;
+
+		const filteredUsers = await userModel.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};

5902968183e8c26e56960dcdfc1aafdee4c8c924
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-08-27 11:26:51 +0100

    getTotalLogsCount

--- models/logSchema.js
+++ models/logSchema.js
@@ -15,6 +15,10 @@ const logSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now }
 });
 
+logSchema.statics.getTotalLogsCount = async function() {
+  return await this.countDocuments();
+};
+
 const Log = mongoose.model('Log', logSchema);
 
 module.exports = Log;
